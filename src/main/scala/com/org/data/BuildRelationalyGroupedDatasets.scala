fa652c1dddc53730d2a1c23489e31f2cc68c9759
Anand Ayyasamy <c-anand.ayyasamy@charter.com>
2018-10-10 19:53:53 -0500

    formatted code

--- src/main/scala/com/org/data/BuildRelationalyGroupedDatasets.scala
+++ src/main/scala/com/org/data/BuildRelationalyGroupedDatasets.scala
@@ -1,16 +1,14 @@
 /*
-# Program      : LDAP_UID_Streaming_Logs.scala
+# Program      : BuildRelationalyGroupedDatasets.scala
 # Date Created : 07/03/2018
-# Description  : This is a Main class for Streaming Job for Unify Identity
-# Parameters   :
-#
+# Description  : This class is used to build the relational datasets
 # Modification history:
 #
 # Date         Author               Description
 # ===========  ===================  ============================================
-# 09/03/2018   Anand Ayyasamy        Creation
+# 10/03/2018   Anand Ayyasamy        Creation
 # ===========  ===================  ============================================
-*/
+ */
 package com.org.data
 import org.apache.spark.sql._
 import org.apache.spark.sql.SparkSession
@@ -19,7 +17,6 @@ import org.apache.spark.sql.functions._
 case class Country(country: String, states: String, city: String)
 
 object BuildRelationalyGroupedDatasets {
-
 
   def main(args: Array[String]): Unit = {
 
@@ -31,44 +28,23 @@ object BuildRelationalyGroupedDatasets {
 
     import spark.implicits._
 
-  /*Read the source data*/
-    val df=spark
-      .read
-      .option("header",true)
-      .csv("groupedDataset.csv").as[Country]
+    /*Read the source data*/
+    val df = spark.read
+      .option("header", true)
+      .csv("groupedDataset.csv")
+      .as[Country]
 
-    df.show()
+    /*Collect each row data and form as Tuples2*/
 
-  /*Collect each row data and form as Tuples2*/
-
-    val dfTuple = df.withColumn("JsonTuple", to_json(struct("*")) )
+    val dfTuple = df.withColumn("JsonTuple", to_json(struct("*")))
 
     /*Group the the datasets by column name and collect the grouped set as a List*/
 
-    val gropedDS = dfTuple.groupBy("country")
+    val gropedDS = dfTuple
+      .groupBy("country")
       .agg(collect_list("JsonTuple").cast("String") as "json_data")
-      .withColumn("country",struct("country").cast("String"))
-
-
-
-
+      .withColumn("country", struct("country").cast("String"))
 
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
